06501b142ff4118fa3901ba5374185b8b9a69b97
Hanumayamma6 <2203031240589@paruluniversity.ac.in>
2026-01-04 11:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (90.10%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,10 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i = 0  # slow pointer
+        i = 0  
 
-        for j in range(len(nums)):  # fast pointer
+        for j in range(len(nums)): 
             if nums[j] != val:
-                nums[i] = nums[j]  # overwrite val
+                nums[i] = nums[j] 
                 i += 1
 
-        return i  # k = number of elements not equal to val
+        return i  
